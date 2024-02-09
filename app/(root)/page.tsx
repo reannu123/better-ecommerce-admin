8838d08d51dfc702629a0edd6d020920aa6ab90f
reannu123 <reannumon123@gmail.com>
2024-02-10 05:31:25 +0800

    Add ClerkProvider

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,10 +1,12 @@
 import Image from "next/image";
 import { Button } from "@/components/ui/button";
 import { UserButton } from "@clerk/nextjs";
-export default function Home() {
+
+export default function SetupPage() {
   return (
-    <p>
-        Hello Admin Dashboard
-    </p>
+    <Button variant="default" size="default">
+      Button
+      <UserButton afterSignOutUrl="/" />
+    </Button>
   );
 }
