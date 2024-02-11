04693188e5e20c5442131349b79b023007c5a7af
reannu123 <reannumon123@gmail.com>
2024-02-11 13:58:30 +0800

    Add ToasterProvider and react-hot-toast for displaying toast messages

--- components/modals/store-modal.tsx
+++ components/modals/store-modal.tsx
@@ -1,9 +1,12 @@
 "use client";
 import * as z from "zod";
+import axios from "axios";
+
 import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useState } from "react";
-import axios from "axios";
+import { toast } from "react-hot-toast";
+
 import { useStoreModal } from "@/hooks/use-store-modal";
 import { Modal } from "@/components/ui/modal";
 import {
@@ -35,12 +38,12 @@ const StoreModal = () => {
     try {
       setLoading(true);
       const response = await axios.post("/api/stores", values);
+      toast.success("Store created.");
     } catch (error) {
-      console.log(error);
+      toast.error("Something went wrong.");
     } finally {
       setLoading(false);
     }
-    console.log(values);
   };
 
   return (
