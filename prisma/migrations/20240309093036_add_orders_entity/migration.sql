9e64f219c2525339a9312769ed734613722563a9
reannu123 <reannumon123@gmail.com>
2024-03-09 17:30:50 +0800

    Added order and order item models

---
+++ prisma/migrations/20240309093036_add_orders_entity/migration.sql
@@ -0,0 +1,27 @@
+-- CreateTable
+CREATE TABLE `Order` (
+    `id` VARCHAR(191) NOT NULL,
+    `storeId` VARCHAR(191) NOT NULL,
+    `isPaid` BOOLEAN NOT NULL DEFAULT false,
+    `phone` VARCHAR(191) NOT NULL DEFAULT '',
+    `address` VARCHAR(191) NOT NULL DEFAULT '',
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL,
+
+    INDEX `Order_storeId_idx`(`storeId`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `OrderItem` (
+    `id` VARCHAR(191) NOT NULL,
+    `orderId` VARCHAR(191) NOT NULL,
+    `productId` VARCHAR(191) NOT NULL,
+    `quantity` INTEGER NOT NULL,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL,
+
+    INDEX `OrderItem_orderId_idx`(`orderId`),
+    INDEX `OrderItem_productId_idx`(`productId`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
