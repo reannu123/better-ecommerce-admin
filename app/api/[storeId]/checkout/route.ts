6967ec7b6d6458c15d219ecd302ef1f2e33d8fe9
reannu123 <reannumon123@gmail.com>
2024-03-11 17:29:08 +0800

    Refactor checkout route and paymongo module

--- app/api/[storeId]/checkout/route.ts
+++ app/api/[storeId]/checkout/route.ts
@@ -1,86 +1,17 @@
 import { NextResponse } from "next/server";
-import { auth } from "@clerk/nextjs";
 import prismadb from "@/lib/prismadb";
-import { sendPaymongo } from "@/lib/paymongo";
-
-interface RequestOptions {
-  line_items?: lineItemsProps[];
-  cancel_url?: string;
-  success_url?: string;
-}
-
-const createOptions = (requestOptions: RequestOptions) => {
-  const defaultOptions = {
-    method: "POST",
-    url: "https://api.paymongo.com/v1/checkout_sessions",
-    headers: {
-      accept: "application/json",
-      "Content-Type": "application/json",
-      authorization: "Basic c2tfdGVzdF9zUXF0c0s0V3JmUXFyYUZtYWN5bld3WGc6",
-    },
-    data: {
-      data: {
-        attributes: {
-          billing: { phone: "09763643131" },
-          send_email_receipt: true,
-          show_description: false,
-          show_line_items: true,
-          cancel_url: "https://google.com",
-          line_items: [
-            { amount: 2000, currency: "PHP", name: "productName", quantity: 1 },
-          ],
-          payment_method_types: [
-            "card",
-            "gcash",
-            "paymaya",
-            "grab_pay",
-            "dob",
-            "dob_ubp",
-          ],
-          success_url: "https://google.com",
-        },
-      },
-    },
-  };
-
-  return {
-    ...defaultOptions,
-    data: {
-      data: {
-        attributes: {
-          ...defaultOptions.data.data.attributes,
-          line_items:
-            requestOptions.line_items ||
-            defaultOptions.data.data.attributes.line_items,
-          cancel_url:
-            requestOptions.cancel_url ||
-            defaultOptions.data.data.attributes.cancel_url,
-          success_url:
-            requestOptions.success_url ||
-            defaultOptions.data.data.attributes.success_url,
-        },
-      },
-    },
-  };
-};
-
-interface lineItemsProps {
-  name: string;
-  quantity?: number;
-  amount: number;
-  currency: "PHP";
-  description?: string;
-  images: string[];
-}
+import { sendPaymongo, createOptions, lineItemsProps } from "@/lib/paymongo";
 
 const corsHeaders = {
   "Access-Control-Allow-Origin": "*",
   "Access-Control-Allow-Methods": "POST, OPTIONS, GET, PUT, DELETE",
   "Access-Control-Allow-Headers": "Content-Type, Authorization",
 };
+
 export async function OPTIONS() {
   return NextResponse.json({}, { headers: corsHeaders });
 }
+
 export async function POST(req: Request) {
   try {
     const { productIds } = await req.json();
@@ -112,12 +43,12 @@ export async function POST(req: Request) {
 
     const options = createOptions({
       line_items,
-      cancel_url: "http://localhost:3001/",
-      success_url: "http://localhost:3001/",
+      success_url: "http://localhost:3001/cart?success=1",
+      cancel_url: "http://localhost:3001/cart?canceled=1",
     });
 
     const response = await sendPaymongo(options);
-    return NextResponse.json(response.data, { headers: corsHeaders });
+    return NextResponse.json(response, { headers: corsHeaders });
   } catch (error) {
     console.log(" [CHECKOUT_POST]", error);
     return new NextResponse("Internal error", {
