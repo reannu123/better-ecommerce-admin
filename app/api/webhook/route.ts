e855cc3b8ae7a1136723dfbaa8a93cde216e1473
reannu123 <reannumon123@gmail.com>
2024-03-23 14:24:04 +0800

    Add getCheckoutSession function to webhook route

--- app/api/webhook/route.ts
+++ app/api/webhook/route.ts
@@ -1,10 +1,12 @@
-import { extractWebhookData } from "@/lib/paymongo";
+import { extractWebhookData, getCheckoutSession } from "@/lib/paymongo";
 import { NextResponse } from "next/server";
 
 export async function POST(req: Request) {
   try {
     const data = await extractWebhookData(req);
     console.log(" [WEBHOOK_POST]", data);
+    const checkout_session_id = data.attributes.data.id;
+    getCheckoutSession(checkout_session_id);
     return NextResponse.json("Webhook Post Success", { status: 200 });
   } catch (error) {
     console.log(" [WEBHOOK_POST]", error);
